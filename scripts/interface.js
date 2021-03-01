0e581f26dd8c26fefdcde5c3bebc3e9146bd4fe3
erhise <erikhidrup@gmail.com>
2021-03-01 07:08:14 +0100

    feat: add actions

--- scripts/interface.js
+++ scripts/interface.js
@@ -2,11 +2,14 @@ const blessed = require('blessed');
 const contrib = require('blessed-contrib');
 const util = require('util');
 
-function buildUi(extras = {}) {
+const DEFAULT_DATA = { actions: [], extras: {} };
+
+function buildUi(data = {}) {
   screen = blessed.screen({
     title: 'Offline Blitz',
   });
 
+  // Create grid
   grid = new contrib.grid({
     rows: 36,
     cols: 12,
@@ -28,26 +31,76 @@ function buildUi(extras = {}) {
         fg: "yellow"
       }
     },
-    commands: buildMenuCommands(extras)
+    commands: buildMenuCommands({
+      ...DEFAULT_DATA.extras,
+      ...data.extras
+    }),
   });
-  //
+
+  // Create list for actions
+  actionList = grid.set(0, 0, 15, 4, blessed.list, {
+    label: 'Actions',
+    parent: screen,
+    keys: true,
+    vi: true,
+    align: 'left',
+    selectedFg: 'white',
+    selectedBg: 'blue',
+    //interactive: false,
+    padding: { left: 1, right: 1 },
+    style: {
+      selected: {
+        fg: "black",
+        bg: "light-yellow"
+      },
+      item: {
+        fg: 'light-blue',
+      },
+    },
+    columnSpacing: 1,
+  });
+  
+  // Create exeuction triggered log
+  executionLog = grid.set(15, 0, 21, 4, contrib.log, {
+    label: 'Execution triggered',
+    parent: screen,
+    fg: 'green',
+    selectedBg: 'blue',
+    padding: { left: 1, right: 1 },
+  });
+  
+  actions = data.actions !== undefined ? data.actions : DEFAULT_DATA.actions;
+  actionList.setItems(actions.map(action => action.title));
+
   //grid.set(row, col, rowSpan, colSpan, obj, opts)
-  log = grid.set(0,0,35,12, contrib.log, {
+  log = grid.set(0,4,35,8, contrib.log, {
+    label: 'Logging output',
+    parent: screen,
     fg: 'green',
     selectedFg: 'green',
-    label: 'Server log',
   });
 
   
-  screen.render()
+  // focus list directly
+  actionList.focus();
+  screen.render();
 }
 
 function buildMenuCommands(extras = {}) {
   let defaultCmds = {
     ' Exit': {
       keys: ['Q-q', 'Q', 'q', 'C-c', 'escape'],
-      callback: () => process.exit(0)
-    }
+      callback: () => process.exit(0),
+    },
+    ' Action': {
+      keys: ['n', 'enter'],
+      callback: () => {
+        if (actions[actionList.selected] !== undefined) {
+          actions[actionList.selected].callback();
+          executionLog.log(`${actions[actionList.selected].title} was triggered`);
+        }
+      },
+    },
   };
   return {...defaultCmds, ...extras};
 }
---
+++ templates/example.template.ts
@@ -0,0 +1,43 @@
+import { merge, NEVER, Subject, timer } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+const { logObject, buildUi } = require('../scripts/interface');
+
+const extras = { 
+  ' Info': {
+    keys: ['i'],
+    callback: () => {
+      logObject(
+        'App.ts include a continous running stream of bananas with a trigger for labour on work and off work.'
+      );
+    }
+  },
+};
+const actions = [
+  { 
+    title: 'Throw in a banana on the band',
+    callback: () => {
+      manualBananas$.next();
+    },
+  },
+  { 
+    title: 'Start/Stop the banana band',
+    callback: () => {
+      bananaBandIsStopped = !bananaBandIsStopped;
+    },
+  },
+];
+buildUi({ actions, extras });
+
+const manualBananas$ = new Subject<void>();
+let numberOfBananas = 0;
+let bananaBandIsStopped = false;
+const frequency$ = timer(0, 3000);
+const runningBand$ = frequency$.pipe(
+  switchMap(() => bananaBandIsStopped ? NEVER : frequency$),
+);
+const bananas$ = merge(runningBand$, manualBananas$).pipe(
+  map(() => numberOfBananas++),
+);
+
+bananas$.subscribe(res => logObject(res));
+
