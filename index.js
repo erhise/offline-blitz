3b7854bd658539d8ca3c21e041fadd4707e60b7a
Simon Gustafsson <simon.gustafsson89@gmail.com>
2021-03-03 11:24:11 +0100

    build: replace nodemon with chokidar

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const { ensureValidPath, fileName } = require('./scripts/filename');
-const nodemon = require('nodemon');
+const chokidar = require('chokidar');
+const child_process = require('child_process');
 
 const path = ensureValidPath(fileName(process.argv[2]));
 
-nodemon(`-I --quiet --config nodemon.json ${path}`);
+let sandbox;
 
-nodemon.on('start', () => {
+function startSandbox() {
   console.clear();
-  console.log('started', new Date().toISOString());
-}).on('quit', () => {
-  //
-}).on('restart', (files) => {
-  console.clear();
-  console.log('restarted', new Date().toISOString(), files);
-}).once('exit', () => {
-  process.exit(0);
+  console.log(new Date(), "sandbox starting");
+  sandbox = child_process.spawn('ts-node', [path], {
+    stdio: 'inherit'
+  });
+  sandbox.on('exit', (exitCode) => {
+    if (exitCode === 130) {
+      console.log(new Date(), "sandbox aborting");
+      process.exit(0);
+    }
+  });
+}
+
+chokidar.watch(path).on('change', () => {
+  sandbox.kill();
+  startSandbox();
 });
+
+startSandbox();
