41cd28563474bd3bb4fecd7b646840fb7c10a236
Simon Gustafsson <simon.gustafsson89@gmail.com>
2021-03-09 07:53:57 +0100

    build: use ts-nodes compiler programmatically to recompile on change

--- index.js
+++ index.js
@@ -1,33 +1,71 @@
 const { ensureValidPath, fileName } = require('./scripts/filename');
-const chokidar = require('chokidar');
-const child_process = require('child_process');
+const { register } = require('ts-node');
+const { watch } = require('chokidar');
+const { readFileSync } = require('fs');
+const { fork } = require('child_process');
 
 const path = ensureValidPath(fileName(process.argv[2]));
 
-function logger(msg) {
-  console.clear();
-  console.log(new Date(), msg);
-}
-
-let sandbox;
-
-function startSandbox() {
-  sandbox = child_process.spawn('ts-node', [path], {
-    stdio: 'inherit'
-  });
-  sandbox.on('exit', (exitCode) => {
-    if (exitCode === 130) {
-      console.log(new Date(), "sandbox aborting");
-      watcher.close().then(() => process.exit(0));
-    }
-  });
-}
-
-const watcher = chokidar.watch(path).on('change', () => {
-  logger('sandbox restarting');
-  sandbox.kill();
-  startSandbox();
+const tsCompiler = register({
+  scope: true,
+  transpileOnly: true,
 });
 
-logger('sandbox starting');
-startSandbox();
+blitz(path, tsCompiler);
+
+function blitz(filePath, compiler) {
+  logger('sandbox starting', filePath);
+  let sandbox = runSandbox(filePath, compiler);
+  watch(filePath).on('change', (changedPath) => {
+    logger('sandbox restarting', changedPath);
+    if (sandbox !== null) {
+      sandbox.kill();
+    }
+    sandbox = runSandbox(changedPath, compiler);
+  });
+}
+
+function logger(...msg) {
+  console.clear();
+  console.log(new Date(), ...msg);
+}
+
+function runSandbox(filePath, compiler) {
+  const code = compile(filePath, compiler);
+  return code !== null ? newSandbox(code) : null;
+}
+
+function compile(filePath, { compile }) {
+  const code = readFileSync(filePath, 'utf-8');
+  try {
+    return compile(code, filePath);
+  } catch (compilationError)  {
+    console.log('Compilation error:\n', compilationError);
+  }
+  return null;
+}
+
+function newSandbox(code) {
+  const sandbox = fork('eval.js');
+  sandbox.on('message', onExecutionError(sandbox));
+  sandbox.on('exit', onSandboxAborted(sandbox));
+  sandbox.send(code);
+  return sandbox;
+}
+
+function onExecutionError(sandbox) {
+  return (executionError) => {
+    console.log('Execution error:\n', executionError);
+    sandbox.kill();
+  };
+}
+
+function onSandboxAborted(sandbox) {
+  return (exitCode) => {
+    if (exitCode === 130) {
+      sandbox.kill();
+      process.exit(0);
+    }
+  };
+}
+
